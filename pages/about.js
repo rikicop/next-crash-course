fd5e166d1e541d5d7232876075917dbe962d1675
rikicop <ruperto1@protonmail.com>
2022-01-18 22:37:38 -0400

    styles...

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>About me!!</title>
+      </Head>
+      <h1>Hello About!!</h1>
+    </div>
+  );
+};
+
+export default about;
