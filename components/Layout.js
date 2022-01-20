af24e1006f94a4851b5998375ec3257128acda63
rikicop <ruperto1@protonmail.com>
2022-01-19 20:46:56 -0400

    style jsx

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,18 @@
+import Nav from "./Nav";
+import Header from "./Header";
 import styles from "../styles/Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>Hello</h1>
-        {children}
-      </main>
-    </div>
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Header />
+          {children}
+        </main>
+      </div>
+    </>
   );
 };
 
